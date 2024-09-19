a30261be087d362c6f80a89c99fc001d1292d65c
LxShadow <152157350+LxShadow@users.noreply.github.com>
2024-09-19 17:55:16 -0600

    𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕

--- plugins/menu-labiblia.js
+++ plugins/menu-labiblia.js
@@ -25,11 +25,10 @@ if (!db.data.chats[m.chat].modohorny && m.isGroup) throw `${tradutor.texto1[0]} 
     const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
     const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
     const document = doc[Math.floor(Math.random() * doc.length)];
-    const str = `╭═══〘 ✯✯✯✯✯✯✯✯✯ 〙══╮
-║    ◉— *𝐓𝐡𝐞 𝐌𝐲𝐬𝐭𝐢𝐜 - 𝐁𝐨𝐭* —◉
-║≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡║
+    const str = `𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕
+    
 ║➤ ${tradutor.texto1[1]} ${taguser}*
-╰═══╡✯✯✯✯✯✯✯✯✯╞═══╯
+
 
 ┏━━━━━━━━━━━━━━━━┓
 ┃ ${tradutor.texto1[2]}
@@ -66,7 +65,7 @@ if (!db.data.chats[m.chat].modohorny && m.isGroup) throw `${tradutor.texto1[0]} 
 ┣ ඬ⃟ 🔞 _${usedPrefix}nsfwglass_
 ┣ ඬ⃟ 🔞 _${usedPrefix}hentaipdf *<texto>*_
 ┣ ඬ⃟ 🔞 _${usedPrefix}hentaisearch *<texto>*_
-┗━━━━━━━━━━━━━━━━┛`.trim();
+┗━━━━━━━𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕━━━━━━━━━┛`.trim();
     if (m.isGroup) {
       await conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: m});
       await conn.sendFile(m.chat, vn, './src/assets/audio/01J673V13NHPW7FA028ZPYC18Q.mp3', null, m, true, {type: 'audioMessage', ptt: true});
