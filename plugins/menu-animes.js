fdc8c894930c9e1d8a8a55a26f4b2bed614fe779
Antonyxit <carlosmorga45@gmail.com>
2024-09-25 13:56:54 -0600

    Update menu-animes.js

--- plugins/menu-animes.js
+++ plugins/menu-animes.js
@@ -25,7 +25,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
     const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
     const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
     const document = doc[Math.floor(Math.random() * doc.length)];
-    const str = `𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕
+    const str = `𝐖𝐈𝐍𝐍𝐈𝐄 𝑩𝒐𝒕
     
     ${tradutor.texto1[1]}, ${taguser}*
 
@@ -73,7 +73,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
 ┣ ඬ⃟ 🧿 _${usedPrefix}sasuke_
 ┣ ඬ⃟ 🧿 _${usedPrefix}sakura_
 ┣ ඬ⃟ 🧿 _${usedPrefix}cosplay_
-┗━━━━━━━━𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕━━━━━━━━┛`.trim();
+┗━━━━━━━━━━━━━━━━┛`.trim();
     if (m.isGroup) {
       // await conn.sendFile(m.chat, vn, './src/assets/audio/01J673Y3TGCFF1D548242AX68Q.mp3', null, m, true, { type: 'audioMessage', ptt: true})
       const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
